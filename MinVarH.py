041e09f869508c60c5c214332a7bcccbc7f2f87c
Julius <135064855+Jrogrobs@users.noreply.github.com>
2024-09-13 00:00:43 +0100

    Update MinVarH.py

--- MinVarH.py
+++ MinVarH.py
@@ -1,12 +1,28 @@
+import pandas as pd
 import numpy as np
 
-def Minimum_Variance_Hedge_Ratio(delta_spot_price, delta_futures_price):
+
+def Minimum_Variance_Hedge_Ratio(spot_price, futures_price, hedge_size, futures_size):
     """
-    Simple function to calculate the Minimum Variance Hedge Ratio.
+    spot_price: spot prices
+    futures_price: futures prices
+    hedge_size: size of the spot position to be hedged
+    futures_size: size of the futures position
+    corr: correlation
+    std: standard deviation
+    h: Hedging ratio
+    N: Optimal number of contracts to hege
     """
-    corr = np.corrcoef(delta_spot_price, delta_futures_price)
-    spot_price_std = np.std(delta_spot_price)
-    futures_price_std = np.std(delta_futures_price)
-    h = corr *(spot_price_std / futures_price_std)
+
+    corr = np.corrcoef(spot_price, futures_price)
+    spot_price_std = np.std(spot_price)
+    futures_price_std = np.std(futures_price)
     
-print(h)
+    h = corr *(spot_price_std / futures_price_std)   
+    
+    N = (h*hedge_size) / futures_size
+    return(h, N)
+    
+    
+    
+    
