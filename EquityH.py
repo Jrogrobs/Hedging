f7e41a094e40ee2cc615a933a31ff46cfbf6d9d3
Julius <135064855+Jrogrobs@users.noreply.github.com>
2024-09-13 00:15:08 +0100

    Update EquityH.py

--- EquityH.py
+++ EquityH.py
@@ -3,8 +3,8 @@ import numpy as np
 def Equity_Hedge(p_spot, p_futures):
     """
     Function to implement an Equity Hedge using stock index futures
-    p_spot: current value of the portfolio
-    p_futures: curent value of futures contract
+    p_spot: Current value of the portfolio
+    p_futures: current value of the futures contract
     N: Hedging Ratio
     """
     
@@ -16,10 +16,10 @@ def Equity_Hedge(p_spot, p_futures):
 def Equity_Hedge_with_Beta(p_spot, p_futures,beta, beta_new):
     """
     Function to implement an Equity Hedge using stock index futures
-    p_spot: current value of the portfolio
-    p_futures: curent value of futures contract
-    beta: current portfolio beta
-    beta_new: new portfolio beta
+    p_spot: Current value of the portfolio
+    p_futures: Current value of the futures contract
+    beta: Current portfolio beta
+    beta_new: New portfolio beta
     N: Hedging Ratio
     """
     if beta > beta_new:
